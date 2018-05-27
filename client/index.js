2700c4004852445a824affb6049725b26cdb197b
Nobuyuki Fujioka <nobuyukifujioka@nobuyukis-mbp.home>
2018-05-27 08:03:06 +0100

    adds user query

--- client/index.js
+++ client/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
+import { Router, hashHistory, Route, IndexRoute } from 'react-router'
+
+import App from './components/App'
 
 const client = new ApolloClient({
   dataIdFromObject: o=> o.id
@@ -9,9 +12,12 @@ const client = new ApolloClient({
 
 const Root = () => {
   return (
-    <div>
-      Auth Starter
-    </div>
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+        </Route>
+      </Router>
+    </ApolloProvider>
   );
 };
 
